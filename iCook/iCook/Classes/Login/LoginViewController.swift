c157fbdcdf2215d7b313dd1d6cefb679e867a7cb
Rada Hristova <rada.hristova@mobilewaves.com>
2021-11-29 17:29:19 +0200

    Commit Google Sign In

--- iCook/iCook/Classes/Login/LoginViewController.swift
+++ iCook/iCook/Classes/Login/LoginViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
 class LoginViewController: ICViewController {
     
@@ -13,11 +15,11 @@ class LoginViewController: ICViewController {
     @IBOutlet weak var iCookLogo: ICRoundedImageView!
     @IBOutlet weak var appNameLabel: ICLabel!
     @IBOutlet weak var descriptionLabel: ICLabel!
-    @IBOutlet weak var signInButton: GIDSignInButton!
     
     //MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         appNameLabel.configureDefault(withSize: 30)
         descriptionLabel.configureDefault(withSize: 26)
     }
@@ -29,94 +31,102 @@ class LoginViewController: ICViewController {
         present(tabBar, animated: true, completion: nil)
     }
     
-    func googleSignIn() {
+    @IBAction func didTouchUpInside(_ button: GIDSignInButton) {
+        googleSignIn()
+    }
+    
+    @objc private func googleSignIn() {
         guard let clientID = FirebaseApp.app()?.options.clientID else { return }
-
+        
         // Create Google Sign In configuration object.
         let config = GIDConfiguration(clientID: clientID)
-
+        
         // Start the sign in flow!
         GIDSignIn.sharedInstance.signIn(with: config, presenting: self) { [unowned self] user, error in
-
-          if let error = error {
-            // ...
-            return
-          }
-
-          guard
-            let authentication = user?.authentication,
-            let idToken = authentication.idToken
-          else {
-            return
-          }
-
-          let credential = GoogleAuthProvider.credential(withIDToken: idToken,
-                                                         accessToken: authentication.accessToken)
-
-          // ...
-            Auth.auth().signIn(with: credential) { authResult, error in
-                if let error = error {
-                  let authError = error as NSError
-                  if isMFAEnabled, authError.code == AuthErrorCode.secondFactorRequired.rawValue {
-                    // The user is a multi-factor user. Second factor challenge is required.
-                    let resolver = authError
-                      .userInfo[AuthErrorUserInfoMultiFactorResolverKey] as! MultiFactorResolver
-                    var displayNameString = ""
-                    for tmpFactorInfo in resolver.hints {
-                      displayNameString += tmpFactorInfo.displayName ?? ""
-                      displayNameString += " "
-                    }
-                    self.showTextInputPrompt(
-                      withMessage: "Select factor to sign in\n\(displayNameString)",
-                      completionBlock: { userPressedOK, displayName in
-                        var selectedHint: PhoneMultiFactorInfo?
-                        for tmpFactorInfo in resolver.hints {
-                          if displayName == tmpFactorInfo.displayName {
-                            selectedHint = tmpFactorInfo as? PhoneMultiFactorInfo
-                          }
-                        }
-                        PhoneAuthProvider.provider()
-                          .verifyPhoneNumber(with: selectedHint!, uiDelegate: nil,
-                                             multiFactorSession: resolver
-                                               .session) { verificationID, error in
-                            if error != nil {
-                              print(
-                                "Multi factor start sign in failed. Error: \(error.debugDescription)"
-                              )
-                            } else {
-                              self.showTextInputPrompt(
-                                withMessage: "Verification code for \(selectedHint?.displayName ?? "")",
-                                completionBlock: { userPressedOK, verificationCode in
-                                  let credential: PhoneAuthCredential? = PhoneAuthProvider.provider()
-                                    .credential(withVerificationID: verificationID!,
-                                                verificationCode: verificationCode!)
-                                  let assertion: MultiFactorAssertion? = PhoneMultiFactorGenerator
-                                    .assertion(with: credential!)
-                                  resolver.resolveSignIn(with: assertion!) { authResult, error in
-                                    if error != nil {
-                                      print(
-                                        "Multi factor finanlize sign in failed. Error: \(error.debugDescription)"
-                                      )
-                                    } else {
-                                      self.navigationController?.popViewController(animated: true)
-                                    }
-                                  }
-                                }
-                              )
-                            }
-                          }
-                      }
-                    )
-                  } else {
-                    self.showMessagePrompt(error.localizedDescription)
-                    return
-                  }
-                  // ...
-                  return
-                }
-                // User is signed in
-                // ...
+            
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard
+                let authentication = user?.authentication,
+                let idToken = authentication.idToken
+            else {
+                return
+            }
+            
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                           accessToken: authentication.accessToken)
+            
+            fireBaseLogin(with: credential)
+        }
+    }
+    
+    private func fireBaseLogin(with credential: AuthCredential) {
+        Auth.auth().signIn(with: credential) { [weak self] authResult, error in
+            if let error = error {
+                self?.mfa(error: error)
+            }
+            else {
+//                self.showMessagePrompt(error.localizedDescription)
             }
         }
     }
+    
+    private func mfa(error: Error) {
+//        let authError = error as NSError
+//        if isMFAEnabled, authError.code == AuthErrorCode.secondFactorRequired.rawValue {
+//            // The user is a multi-factor user. Second factor challenge is required.
+//            let resolver = authError
+//                .userInfo[AuthErrorUserInfoMultiFactorResolverKey] as! MultiFactorResolver
+//            var displayNameString = ""
+//            for tmpFactorInfo in resolver.hints {
+//                displayNameString += tmpFactorInfo.displayName ?? ""
+//                displayNameString += " "
+//            }
+//            self.showTextInputPrompt(
+//                withMessage: "Select factor to sign in\n\(displayNameString)",
+//                completionBlock: { userPressedOK, displayName in
+//                    var selectedHint: PhoneMultiFactorInfo?
+//                    for tmpFactorInfo in resolver.hints {
+//                        if displayName == tmpFactorInfo.displayName {
+//                            selectedHint = tmpFactorInfo as? PhoneMultiFactorInfo
+//                        }
+//                    }
+//                    PhoneAuthProvider.provider()
+//                        .verifyPhoneNumber(with: selectedHint!, uiDelegate: nil,
+//                                           multiFactorSession: resolver
+//                                            .session) { verificationID, error in
+//                            if error != nil {
+//                                print(
+//                                    "Multi factor start sign in failed. Error: \(error.debugDescription)"
+//                                )
+//                            } else {
+//                                self.showTextInputPrompt(
+//                                    withMessage: "Verification code for \(selectedHint?.displayName ?? "")",
+//                                    completionBlock: { userPressedOK, verificationCode in
+//                                        let credential: PhoneAuthCredential? = PhoneAuthProvider.provider()
+//                                            .credential(withVerificationID: verificationID!,
+//                                                        verificationCode: verificationCode!)
+//                                        let assertion: MultiFactorAssertion? = PhoneMultiFactorGenerator
+//                                            .assertion(with: credential!)
+//                                        resolver.resolveSignIn(with: assertion!) { authResult, error in
+//                                            if error != nil {
+//                                                print(
+//                                                    "Multi factor finanlize sign in failed. Error: \(error.debugDescription)"
+//                                                )
+//                                            } else {
+//                                                self.navigationController?.popViewController(animated: true)
+//                                            }
+//                                        }
+//                                    }
+//                                )
+//                            }
+//                        }
+//                }
+//            )
+//        }
+    }
+    
 }
--- iCook/iCook/Utils/AppDelegate.swift
+++ iCook/iCook/Utils/AppDelegate.swift
@@ -12,17 +12,17 @@ import GoogleSignIn
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-   
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+     
         UINavigationBar.appearance().tintColor = .icAccentColor
     
         return true
     }
+    
+  
 
     // MARK: UISceneSession Lifecycle
 
